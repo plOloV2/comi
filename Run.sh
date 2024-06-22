a3e13dfecc8cc4862780652e59d89f7839142674
pl_Olov2 <ak.dziagwa@gmail.com>
2024-06-22 04:32:40 +0200

    error handling added

--- Run.sh
+++ Run.sh
@@ -8,6 +8,22 @@ comp_time() {
     return ${exit_code}
 }
 
+errors() {
+    local error_code=$?
+    if [ $error_code -eq 0 ]; then
+       echo -e "\nExecuted succesfuly"
+    else
+         if [ $error_code -eq 1 ]; then
+            echo "Error: argc not equal 2. Program returned with code 1."
+        elif [ $error_code -eq 2 ]; then
+            echo "Error: prepare_data() error. Program returned with code 2."
+        elif [ $error_code -eq 3 ]; then
+            echo "Error: future error. Program returned with code 3."
+        fi
+    fi
+}
+
+
 # Set the output binary name
 OUTPUT_BINARY="comi"
 
@@ -25,6 +41,7 @@ if [ $? -eq 0 ]; then
     if [ -f "$BIN_FILES_LOCATION/$OUTPUT_BINARY" ]; then
         echo -e "\nRunning the program with args: $@\n"
         ./"$BIN_FILES_LOCATION/$OUTPUT_BINARY" "$@"
+        errors
     else
         echo "Error: Compiled binary '$BIN_FILES_LOCATION/$OUTPUT_BINARY' not found!"
         exit 1
