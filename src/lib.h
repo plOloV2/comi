701531eabc348ae8a45135182f5f5a02d26d94e6
pl_Olov2 <ak.dziagwa@gmail.com>
2024-06-23 02:49:21 +0200

    Data preparation done

--- src/lib.h
+++ src/lib.h
@@ -4,15 +4,18 @@
 #include <memory.h>
 #include <omp.h>
 
+#define DBL_MAX 9223372036854775807;
+
 struct Data{
     unsigned char pointsNum;
     double* dist;
 };
 
 struct Thread{
-    double best;
-    bool* visited;
-    unsigned char visited_count;
+    double Best;
+    bool* Visited;
+    unsigned char VisitedCount;
+    unsigned char Now;
 };
 
 int base(int i, int a, int j);                      //calculates positon of distance in dist array
@@ -91,19 +94,69 @@ struct Data* PrepareData(char* FileName){
 }
 
 double CalculateStartDist(struct Data* data){
-    double result = 0;
+    double result = 0, smallest = DBL_MAX;
+    int best, now, HowMany;
+    bool beenThere[data->pointsNum];
 
+    for(int i = 1; i < data->pointsNum; i++)
+        beenThere[i] = false;
 
-    return result = 0;
+    beenThere[0] = true;
+    now = 0;
+    HowMany = 1;
+
+    while(HowMany < data->pointsNum){
+        for(int i = 0; i < data->pointsNum; i++){
+            if(beenThere[i] == 0 && data->dist[base(now, data->pointsNum, i)] < smallest){
+                smallest = data->dist[base(now, data->pointsNum, i)];
+                best = i;
+            }
+        }
+
+        result += smallest;
+        beenThere[best] = true;
+        now = best;
+        HowMany++;
+        smallest = DBL_MAX;
+
+    }
+    
+
+    return result + data->dist[base(0, data->pointsNum, now)];
 }
 
 struct Thread** PrepareThreads(double StartDistance, unsigned char NumOfPoints){
+    struct Thread** result = (struct Thread**) malloc(sizeof(struct Thread*)*8);
+    if(result == NULL)
+        return NULL;
 
-    return NULL;
+    for(int i = 0; i < 8; i++){
+        result[i] = (struct Thread*) malloc(sizeof(double) + sizeof(bool*) + sizeof(char)*2);
+        if(result[i] == NULL)
+            return NULL;
+
+        result[i]->Best = StartDistance;
+        result[i]->VisitedCount = 1;
+        result[i]->Now = 0;
+        result[i]->Visited = (bool*) malloc(sizeof(bool)*NumOfPoints);
+        if(result[i]->Visited == NULL)
+            return NULL;
+        
+        for(int j = 1; j < NumOfPoints; j++)
+            result[i]->Visited[j] = false;
+
+        result[i]->Visited[0] = true;
+    }
+
+    return result;
 }
 
 void PrintStartStats(struct Data* data, double StartDistance){
 
     printf("Number of points: %d\nNumber of all roads: %d\nNot optimized distance: %f\n", data->pointsNum, (data->pointsNum*(data->pointsNum-1))/2, StartDistance);
 
+}
+
+void CalculateBestRoad(){
+    
 }
